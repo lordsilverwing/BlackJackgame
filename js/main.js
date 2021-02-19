76d6eb46ecf08dbc2bb6061f11ac8041603f6ee7
Shane Peterson <speterson@Shanes-MacBook-Pro.local>
2021-02-18 17:48:52 -0700

    add psuedo code

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// set global variables
+
+//cached element references
+    //card values
+    
+
+
+// setup init
+    // generate deck of cards
+    //initiates deal
+    //redeal function in event of loss or win
+    //deck is always regenerated each deal
+
+
+// set up deal function
+    // player dealt two cards
+    // computer dealt one visible, one hidden card
+// set up hit function
+    //draws a card when used 
+// player hand calculates if 21 or over
+    // if over 21 auto lose
+    // utilize hit function is not 21 and over
+    //when hold intiates dealer
+// set up dealer
+    //dealer reveals hidden card, calcualtes if total 21 or > 15
+    // if lower than 15 dealer hits
+    // if both dealer and player have some number = draw
+
+
